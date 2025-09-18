6d585bac0ae238023f61f41934cd06099d8e27a8
Joao Victor <nightface777@gmail.com>
2025-09-17 22:48:10 -0300

    Save1

---
+++ apideproduto/apideproduto/src/test/java/com/apiprodutos/apideproduto/ApiDeProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiprodutos.apideproduto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDeProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
